ecd6d2a7863ed88736dc729c4c16b7f838f4a58e
Narcissist7 <aa.abuelezz@outlook.com>
2024-10-30 23:27:57 +0300

    Job Controllers so far

--- demo/src/main/java/com/example/demo/Controller/JobController.java
+++ demo/src/main/java/com/example/demo/Controller/JobController.java
@@ -6,6 +6,8 @@ import com.example.demo.ServiceImpl.JobServiceImpl;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("api/job")
@@ -21,16 +23,52 @@ public class JobController {
         return jobService.getAllJob();
     }
 
-    @PostMapping("")
+    @GetMapping("{id}")
+    public Optional<jobs> getJobById(@PathVariable long id){
+        return jobService.getJobById(id);
+    }
+
+    @PostMapping
     public String createJob(@RequestBody jobs job){
         jobService.createJob(job);
         return "Job created";
     }
 
-//    @DeleteMapping("{id}")
-//    public String deleteJob(@PathVariable long id){
-//        jobService.deleteJobById(id);
-//        return "Job deleted";
+    @PostMapping("s")
+    public String createJobs(@RequestBody List<jobs> jobsList) {
+        for (jobs job : jobsList) {
+            jobService.createJob(job);
+        }
+        return "Jobs created";
+    }
+// commented functions require fixing
+
+//    @PostMapping("upsert/{id}")
+//    public void addUpdateJob(@PathVariable Long id, @RequestBody jobs job){
+//        jobService.addUpdateJob(id, job.getTitle(), job.getDescription(), job.getRecruiter());
 //    }
+
+//    @PutMapping("{id}")
+//    public void updateJob(@PathVariable long id,
+//                              @RequestParam(required = false) String title,
+//                              @RequestParam(required = false) String description,
+//                              @RequestParam(required = false) String recruiter){
+//        jobService.updateJob(id, title, description, recruiter);
+//    }
+
+    //update column with out too much json hustle
+//    aka { "z": new z } instead of {"x": x, "Y": y, "Z": new z}
+    @PatchMapping("{id}")
+    public void patchJob(@PathVariable long id, @RequestBody Map<String, Object> updatedJob){
+        jobService.patchJob(id, updatedJob);
+    }
+
+    @DeleteMapping("{id}")
+    public String deleteJob(@PathVariable long id){
+        jobService.deleteJob(id);
+        return "Job deleted";
+    }
+
+
 
 }
