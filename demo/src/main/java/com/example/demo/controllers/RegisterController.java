594985fffc50ad28212aeeb2ac2e6631f5b56914
yassin mohamed abdelmoniem <yassinmohamed022@gmail.com>
2024-11-04 14:46:27 +0200

    securityV2

--- demo/src/main/java/com/example/demo/controllers/RegisterController.java
+++ demo/src/main/java/com/example/demo/controllers/RegisterController.java
@@ -8,12 +8,14 @@ import com.example.demo.services.UserDetailsService;
 import com.example.demo.services.UserService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Collections;
 import java.util.List;
 
 @RestController
@@ -51,16 +53,19 @@ public class RegisterController {
     }
 
     @PostMapping("/login")
-    public String login(@RequestBody UserRequestDTO userRequestDTO) {
-
-        Authentication authentication = authenticationManager.authenticate
-                (new UsernamePasswordAuthenticationToken(userRequestDTO.getEmail(),userRequestDTO.getPassword()));
+    public ResponseEntity<?> login(@RequestBody UserRequestDTO userRequestDTO) {
+        System.out.println(userRequestDTO.getPassword());
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(userRequestDTO.getEmail(), userRequestDTO.getPassword())
+        );
 
         if (authentication.isAuthenticated()) {
-            return jwtService.generateToken(userRequestDTO.getEmail()) ;
-        }else {
-            return "failure";
+            String token = jwtService.generateToken(userRequestDTO.getEmail());
+            return ResponseEntity.ok(Collections.singletonMap("token", token)); // Return as JSON
+        } else {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("failure");
         }
     }
+
 
 }
